c19adfde511b84b0268ed0b2bfcbbbcedcfcd369
rayen12345 <54586143+rayen12345@users.noreply.github.com>
2019-09-02 20:56:36 +0100

    Add files via upload

---
+++ pong/Ball.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 30;
+    public float hitfactor(Vector2 ballposition, Vector2 racketposition, float racketheight)
+    {
+        return (ballposition.y - racketposition.y) / racketheight;
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        float y = hitfactor(transform.position,
+                           collision.transform.position,
+                           collision.collider.bounds.size.y);
+        Vector2 dir = new Vector2(1, y).normalized;
+        GetComponent<Rigidbody2D>().velocity = dir * speed;
+
+        if (collision.gameObject.name == "LeftRacket")
+        {
+
+
+            y = hitfactor(transform.position,
+                                collision.transform.position,
+                                collision.collider.bounds.size.y);
+
+
+            Vector2 direction = new Vector2(-1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        if (collision.gameObject.name == "RightRacket")
+        {
+
+
+            y = hitfactor(transform.position,
+                               collision.transform.position,
+                               collision.collider.bounds.size.y);
+
+
+            Vector2 direction = new Vector2(-1, y).normalized;
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+
+        }
+
+       
+
+        }
+    }
+
---
+++ pong/movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 15;
+    public string axis;
+    void FixedUpdate()
+    {
+
+        float v = Input.GetAxisRaw(axis) * speed;
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, v);
+
+      
+    }
+}
+
+
